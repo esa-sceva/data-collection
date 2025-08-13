fff56584d64c88be764ad16a9219fd97535d5310
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-08-13 02:26:13 +0200

    fixing IOPScraper

--- scraper/iop_scraper.py
+++ scraper/iop_scraper.py
@@ -8,6 +8,10 @@ from scraper.base_url_publisher_scraper import BaseUrlPublisherSource, BaseUrlPu
 
 
 class IOPScraper(BaseUrlPublisherScraper):
+    def __init__(self):
+        super().__init__()
+        self.__next_issue_tag = None
+
     @property
     def config_model_type(self) -> Type[BaseUrlPublisherConfig]:
         return BaseUrlPublisherConfig
@@ -22,25 +26,19 @@ class IOPScraper(BaseUrlPublisherScraper):
             if pdf_tag_issue_list is not None:
                 pdf_tag_list.extend(pdf_tag_issue_list)
 
-            # now, find the link to the next issue or collection
-            scraper = self._scrape_url(current_source.url)
-
-            next_issue_tag = scraper.find(
-                "a",
-                class_=lambda cls: cls and "ml-1" in cls,
-                href=lambda href: href and "/issue/" in href,
-            )
-            if next_issue_tag:
-                next_issue_url = get_scraped_url_by_bs_tag(next_issue_tag, self._config_model.base_url)
-                current_source.url = next_issue_url
-                self._logger.info(f"Next issue URL: {current_source.url}")
-            else:
+            if not self.__next_issue_tag:
                 self._logger.info("No more issues or collections found.")
                 break
+
+            current_source.url = get_scraped_url_by_bs_tag(self.__next_issue_tag, self._config_model.base_url)
+            self._logger.info(f"Next issue URL: {current_source.url}")
+
+        return pdf_tag_list
 
     def _scrape_issue_or_collection(self, source: BaseUrlPublisherSource) -> ResultSet | None:
         self._logger.info(f"Processing Issue / Collection {source.url}")
 
+        self.__next_issue_tag = None
         try:
             scraper = self._scrape_url(source.url)
 
@@ -49,6 +47,13 @@ class IOPScraper(BaseUrlPublisherScraper):
                     "a", href=lambda href: href and "/article/" in href and "/pdf" in href
             )):
                 self._save_failure(source.url)
+
+            # now, find the link to the next issue or collection
+            self.__next_issue_tag = scraper.find(
+                "a",
+                class_=lambda cls: cls and "ml-1" in cls,
+                href=lambda href: href and "/issue/" in href,
+            )
 
             self._logger.debug(f"PDF links found: {len(pdf_tag_list)}")
             return pdf_tag_list
